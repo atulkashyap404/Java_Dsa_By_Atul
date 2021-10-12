06e021cb7b5ca8260a3a349f31000b30d8296c93
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2021-10-12 21:50:01 +0530

    Question how may armstrong in bw 100 to 1000

--- src/com/JavaDsaByAtul/QuestionArmstrong.java
+++ src/com/JavaDsaByAtul/QuestionArmstrong.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 
 public class QuestionArmstrong {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(isArmstrong(n));
+        //Scanner sc = new Scanner(System.in);
+        //int n = sc.nextInt();
+        //System.out.println(isArmstrong(n));
+        for (int i = 100; i < 1000; i++){
+            if(isArmstrong(i)){
+                System.out.print(i + " ");
+            }
+        }
     }
     static boolean isArmstrong(int n){
         int original = n;
